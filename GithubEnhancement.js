681dcdeeb949eac75c68b0a488dd1f251178857e
Neoxy <1007050740@qq.com>
2023-08-13 14:32:59 +0800

    Create GithubEnhancement.js

---
+++ GithubEnhancement.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Github Enhancement
+// @namespace    neoxycn.github.io
+// @version      0.1
+// @description  Github Enhancement
+// @author       Neoxy
+// @match        *://github.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @homepageURL  https://github.com/NeoxyCN/GithubEnhancement
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    //Private span color
+    const elements = document.querySelectorAll('span');
+    elements.forEach(element => {
+        if (element.textContent === 'Private') {
+            element.style.color = '#FF0000';
+        }
+    });
+})();
